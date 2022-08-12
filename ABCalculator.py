716fc7740bb6c4a5f6afc275dcbc0f1355508369
lipirika <lipirika@mail.ru>
2022-08-12 10:45:38 +0300

    Добавление файла программы калькулятора

---
+++ ABCalculator.py
@@ -0,0 +1,11 @@
+# А/В калькулятор
+
+import tkinter as tk
+
+# Создание главного окна
+root = tk.Tk()
+root.geometry("280x300")
+root.title("А/В калькулятор")
+
+# Запуск цикла mainloop
+root.mainloop()
